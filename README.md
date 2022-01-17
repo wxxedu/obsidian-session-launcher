8301bd070196148c85e7d0339b914b26fc263143
wxxedu <36021591+wxxedu@users.noreply.github.com>
2022-01-17 12:08:21 +0800

    Added Readme

--- README.md
+++ README.md
@@ -1,29 +1,19 @@
-## Obsidian Plugin Template
+# Obsidian Session Launcher
 
-This templates includes a barebone directory structure for an Obsidian Plugin. Bundling is based on [Rollup.js](https://rollupjs.org/).
+This plugin launches [Session](https://www.stayinsession.com/) app from Obsidian. 
 
-**NOTE:** This is not the official template. You can find [the official sample plugin here](https://github.com/obsidianmd/obsidian-sample-plugin). This is a plugin template I customized with a nicer (for me) directory structure and some additional tool (like featuring automatic releases with Github's Actions).
+## Usage
 
-## How to use it
+Create a codeblock named session
 
-You can click on the "Use this template" button above.
-
-### Enable Github's Actions
-
-After you created a new repository, remember to rename the `RENAME.github` to `.github` to enable the GitHub's Actions. Then go in `.github/workflows/release.yml` and change `PLUGIN_NAME` on line 10.
-
-## How to compile the plugin
-
-First, install the dependencies with
-
-```bash
-npm i
+````
+```session
+Intent: ...
+CategoryName: ...
+Count: 0
+Duration: 25
 ```
+````
 
-Then, you can compile the plugin with:
+You can also use the editor command that's included to insert this block, which by default should use the hotkeys shift + comand + S. 
 
-```bash
-npm run build
-```
-
-This will create a `main.js` file in the project root. That is the entry point of your plugin.
--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,149 @@
-import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { App, Editor, FileManager, MarkdownEditView, MarkdownView, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { Session } from "apple-session-launcher";
+import * as path from "path";
 
 export default class MyPlugin extends Plugin {
   // This field stores your plugin settings.
-  setting: MyPluginSettings;
 
   onInit() {}
 
   async onload() {
     console.log("Plugin is Loading...");
 
-    // This snippet of code is used to load pluging settings from disk (if any)
-    // and then add the setting tab in the Obsidian Settings panel.
-    // If your plugin does not use settings, you can delete these two lines.
-    this.setting = (await this.loadData()) || {
-      someConfigData: 1,
-      anotherConfigData: "defaultValue",
-    };
-    this.addSettingTab(new MyPluginSettingsTab(this.app, this));
+    this.addCommand({
+      id: "apple-session-launcher-insert-snippet",
+      name: "Insert Session Code",
+      hotkeys: [{modifiers: ["Mod", "Shift"], key: "S"}],
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        editor.replaceSelection(`
+        \`\`\`session
+        Intent: 
+        CategoryName:
+        Duration: 25
+        Count: 0
+        Summary: 
+        \`\`\`
+        `);
+      }
+    })
+
+    this.registerMarkdownCodeBlockProcessor("session", (source, el, ctx) => {
+      
+      let intent = source.split("Intent:")[1];
+      // if intent exists
+      if (intent) {
+        intent = intent.split("\n")[0];
+      }
+      let duration = source.split("Duration:")[1];
+      // if duration exists
+      if (duration) {
+        duration = duration.split("\n")[0];
+      } 
+      let categoryId = source.split("CategoryId:")[1];
+      // if categoryId exists
+      if (categoryId) {
+        categoryId = categoryId.split("\n")[0];
+      } 
+      let categoryName = source.split("CategoryName:")[1];
+      // if categoryName exists
+      if (categoryName) {
+        categoryName = categoryName.split("\n")[0];
+        if (categoryName) {
+          categoryName = categoryName.trim();
+          console.log(categoryName);
+          // if categoryName starts with a #
+          if (categoryName.startsWith("#")) {
+            categoryName = categoryName.substring(1);
+            console.log(categoryName);
+          }
+        }
+      } 
+      console.log(intent, duration, categoryId, categoryName);
+
+      // find out the content after "Summary:" to the end of the file
+      let summary = source.split("Summary:")[1];
+      if (summary) {
+        summary = summary.split("\n")[0];
+      } 
+
+      let sessionCount = source.split("Count:")[1];
+      if (sessionCount) {
+        sessionCount = sessionCount.split("\n")[0];
+      } else {
+        // write "Count: " to the source
+        // source = source + "Count: 0\n";
+        sessionCount = "0";
+      }
+      // create a button to start the session
+      const button = document.createElement("button");
+      button.innerText = `${(Number(duration) > 0 ? duration : 25)} Min.`;
+      button.addEventListener("click", () => {
+        const session = new Session();
+        if (intent) {
+          session.setIntent(intent);
+        }
+        if (duration) {
+          session.setDuration(Number(duration));
+        }
+        if (categoryId) {
+          session.setCategoryId(categoryId);
+        }
+        if (categoryName) {
+          session.setCategoryName(categoryName);
+        }
+      });
+      button.className = "session-button";
+      
+      // create a span for the intent
+      const intentSpan = document.createElement("span");
+      intentSpan.innerText = intent;
+      intentSpan.className = "session-intent";
+
+      // create a span for the count
+      const countSpan = document.createElement("span");
+      countSpan.innerText = sessionCount.trim();
+      countSpan.className = "session-count";
+
+      // create a link for the category
+      const categoryLink = document.createElement("a");
+      categoryLink.innerText = categoryName;
+      categoryLink.href = `obsidian://search?query=${encodeURIComponent(`#${categoryName}`)}`;
+      categoryLink.className = "session-category";
+
+      // create a div for the summary
+      const summaryDiv = document.createElement("div");
+      summaryDiv.innerText = summary;
+      summaryDiv.className = "session-summary";
+
+      const div = document.createElement("div");
+      div.className = "session-div";
+
+      // make button align to the right
+      button.style.float = "right";
+      // add category to the div if category exists
+      if (categoryName) {
+        div.appendChild(categoryLink);
+      }
+
+      // add intent to the div if intent exists
+      if (intent) {
+        div.appendChild(intentSpan);
+      }
+      
+      // add count to the div
+      div.appendChild(countSpan);
+      // add button to the div
+      div.appendChild(button);
+      // add summary to the div if summary exists
+      if (summary) {
+        div.appendChild(summaryDiv);
+      }
+      // add div to the element
+      el.appendChild(div);
+    });
   }
 
   onunload() {
     console.log("Plugin is Unloading...");
-  }
-}
-
-/**
- * This is a data class that contains your plugin configurations. You can edit it
- * as you wish by adding fields and all the data you need.
- */
-interface MyPluginSettings {
-  someConfigData: number;
-  anotherConfigData: string;
-}
-
-class MyPluginSettingsTab extends PluginSettingTab {
-  plugin: MyPlugin;
-
-  constructor(app: App, plugin: MyPlugin) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
-
-  display(): void {
-    const { containerEl } = this;
-    const settings = this.plugin.setting;
-    // This is just an example of a setting controll.
-    new Setting(containerEl)
-      .setName("Setting Name")
-      .setDesc("Setting description")
-      .addText((text) =>
-        text.setValue(String(settings.someConfigData)).onChange((value) => {
-          if (!isNaN(Number(value))) {
-            settings.someConfigData = Number(value);
-            this.plugin.saveData(settings);
-          }
-        })
-      );
   }
 }
